a628cae2aa4f2846f9b7aaa7105fcb922e59c98b
davidtoska <david.toska@outlook.com>
2022-05-19 20:27:40 +0200

    init firebase hosting

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { data } from '../data';
 
 @Component({
   selector: 'liks-playground-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'playground';
+
+  ngOnInit(): void {
+    console.log(data);
+  }
 }
